7d34ec6ad573709e1f89e1c6cb84ea173bacd6dd
Revolver <revolver30000@ya.ru>
2018-11-20 16:58:42 +0400

    небольшие изменения

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -18,3 +18,16 @@ $(function(){
 		return false;
 	});
 });
+
+
+$(function(){
+$('#menuBtn').on('click', function() {
+    if ($('#menuBtn').hasClass('closed')) {
+        $("#menuBtn").toggleClass("closed opened")
+        $('#menuContainer').toggleClass('closed opened');
+    } else if ($('#menuBtn').hasClass('menu-icon opened')) {
+        $("#menuBtn").toggleClass("opened closed")
+        $('#menuContainer').toggleClass('opened closed');
+    }
+})
+});
