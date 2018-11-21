e6c412894530bddf9708f7fbec26fb311a83753c
Revolver <revolver30000@ya.ru>
2018-11-21 23:51:08 +0400

    бургер, цвета к ссылка, правка кнопок

--- src/common.blocks/js/tabs.js
+++ src/common.blocks/js/tabs.js
@@ -2,16 +2,16 @@ $(function(){
 
 	$(".main-content").on("mouseover", ".j-main-content__item", function(){
 
-		var $tabs = $('.j-main-content__item');
-        var $cont = $(".j-main-content__poster");
+		var $items = $('.j-main-content__item');
+        var $posters = $(".j-main-content__poster");
         var $numbers = $(".j-main-content__number");
 
-		$tabs.removeClass("main-content__item_type_active-bg");
-        $cont.removeClass("main-content__poster_type_active-poster");
+		$items.removeClass("main-content__item_type_active-bg");
+        $posters.removeClass("main-content__poster_type_active-poster");
         $numbers.removeClass("main-content__number_type_active-number")
         
 		$(this).addClass("main-content__item_type_active-bg");
-        $cont.eq($(this).index()).addClass("main-content__poster_type_active-poster");
+        $posters.eq($(this).index()).addClass("main-content__poster_type_active-poster");
         $numbers.eq($(this).index()).addClass(" main-content__number_type_active-number");
 
 		return false;
