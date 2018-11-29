4db24f4a62a958354ebd047ee15fedf1a7ada085
Revolver <revolver30000@ya.ru>
2018-11-29 10:29:32 +0400

    настроил минификацию css и сжатие js

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ var concat       = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
 var gls          = require('gulp-live-server');
 var imagemin     = require('gulp-imagemin');
+var cssmin       = require('gulp-cssmin');
+var rename       = require('gulp-rename');
+var uglify       = require('gulp-uglify');
+var pump         = require('pump');
 
 /* Task to compile less */
 gulp.task( 'compile-less', function() {
@@ -14,6 +18,8 @@ gulp.task( 'compile-less', function() {
 			browsers: [ 'last 5 versions' ],
 			cascade: false
 		  }))
+		.pipe( cssmin() )
+        .pipe( rename({suffix: '.min'}) )
 		.pipe( gulp.dest( 'public/css/' ) );
 });
 
@@ -25,12 +31,22 @@ gulp.task( 'watch-less', function() {
 gulp.task( 'copy-img', function(){
 	return gulp.src('src/common.blocks/**/*.{png,jpg}')
 	.pipe(gulp.dest('public/img/'));
-  });
+});
  
 gulp.task( 'concat-js', function() {
 		return gulp.src('src/common.blocks/js/*.js')
 		.pipe(concat('all.js'))
-		.pipe(gulp.dest('public/js/'));
+		.pipe(gulp.dest('src/bundle/'));
+});
+ 
+gulp.task( 'compress-js', function (cb) {
+  pump([
+        gulp.src('src/bundle/*.js'),
+		uglify(),
+		gulp.dest('public/js/')
+    ],
+    cb
+  );
 });
 
 gulp.task( 'live-server', function() {
@@ -41,9 +57,9 @@ gulp.task( 'live-server', function() {
 	gulp.watch([ 'public/**/*.css', 'public/**/*.html' ], function (file) {
 	 server.notify.apply(server, [file]);
 	});
- });
+});
 
-gulp.task( 'compress', function() {
+gulp.task( 'compress-css', function() {
     gulp.src( 'public/img/**/*.{png,jpg}' )
     .pipe(imagemin({
 	  progressive: true
@@ -52,4 +68,4 @@ gulp.task( 'compress', function() {
 });
 
 /* Task when running `gulp` from terminal */
-gulp.task( 'default', [ 'compile-less', 'watch-less', 'concat-js', 'copy-img', 'live-server', 'compress' ]);
+gulp.task( 'default', [ 'compile-less', 'watch-less', 'concat-js', 'compress-js',  'copy-img', 'live-server', 'compress-css' ]);
