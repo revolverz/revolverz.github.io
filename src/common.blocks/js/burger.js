9b23c0cc30cbd765c7d7fcc9665da6e4e09f91df
ishmuratov <ev.ishmuratov@gmail.com>
2018-12-09 16:19:20 +0400

    гамбургер для разрешения sm

--- src/common.blocks/js/burger.js
+++ src/common.blocks/js/burger.js
@@ -1,5 +1,11 @@
 $( function () {
-	$( '.burger' ).click( function (){
-		$( this ).toggleClass( 'open' );
-	});
+$('#menuBtn').on('click', function() {
+	if ($('#menuBtn').hasClass('closed')) {
+		$("#menuBtn").toggleClass("closed opened")
+		$('#menuContainer').toggleClass('closed opened');
+	} else if ($('#menuBtn').hasClass('menu-icon opened')) {
+		$("#menuBtn").toggleClass("opened closed")
+		$('#menuContainer').toggleClass('opened closed');
+	}
+})
 });
