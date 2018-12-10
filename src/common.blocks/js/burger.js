6e61829c7ca219e1fca8657f7e8e54227dbed472
ishmuratov <ev.ishmuratov@gmail.com>
2018-12-11 00:18:17 +0400

    мелкие правки

--- src/common.blocks/js/burger.js
+++ src/common.blocks/js/burger.js
@@ -1,11 +1,11 @@
 $( function () {
-$('#menuBtn').on('click', function() {
-	if ($('#menuBtn').hasClass('closed')) {
-		$("#menuBtn").toggleClass("closed opened")
-		$('#menuContainer').toggleClass('closed opened');
-	} else if ($('#menuBtn').hasClass('menu-icon opened')) {
-		$("#menuBtn").toggleClass("opened closed")
-		$('#menuContainer').toggleClass('opened closed');
-	}
-})
-});
+    $('#menuBtn').on('click', function() {
+        if ($('#menuBtn').hasClass('closed')) {
+            $("#menuBtn").toggleClass("closed opened")
+            $('#menuContainer').toggleClass('closed opened');
+        } else if ($('#menuBtn').hasClass('menu-icon opened')) {
+            $("#menuBtn").toggleClass("opened closed")
+            $('#menuContainer').toggleClass('opened closed');
+        }
+    })
+    });
